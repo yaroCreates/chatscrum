090ce57ef313b0194f0102ab5fca5911ff9e49a4
James Yaro <onuhjamesyaro@gmail.com>
2022-09-13 22:37:05 +0100

    create the sign in component

---
+++ src/components/sign-in/sign-in.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import './sign-in.css'
+
+function SignIn() {
+    return (
+        <div className='sign-in'>
+            <div className="sign-up-header">
+                <h1>Have an account already?</h1>
+                <h3>Sign in here!</h3>
+
+                <form className='form'>
+                    <div className="form-field">
+                        <label for='email'>Email</label>
+                        <input name='email' type='email' />
+                    </div>
+                    <div className="form-field">
+                        <label for='password'>Password</label>
+                        <input name='password' type='password' />
+                    </div>
+                    <div className="form-field">
+                        <label for='text'>Project name</label>
+                        <input name='text' type='text' />
+                    </div>
+                    <select id="options">
+                        <option value="Developer">Developer</option>
+                        <option value="Owner">Owner</option>
+                    </select>
+                    <br />
+                    <button>Sign in</button>
+                </form>
+                <p>Don't have an account? Sign up</p>
+            </div>
+        </div>
+    )
+}
+
+export default SignIn
