2fce534288d503bb6ad3810cdb8f03b44c5d72b2
James Yaro <onuhjamesyaro@gmail.com>
2022-09-13 22:37:35 +0100

    link the sign in component to the root component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './App.css';
 import SignUp from './components/sign-up/sign-up';
+import SignIn from './components/sign-in/sign-in';
 
 class App extends React.Component {
   render() {
