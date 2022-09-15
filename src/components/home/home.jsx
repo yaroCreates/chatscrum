e3726cb8fe3d9d0d424703a902e21f1aafbbf500
James Yaro <onuhjamesyaro@gmail.com>
2022-09-15 23:36:19 +0100

    fix: remove span from home header

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -5,7 +5,7 @@ import {Link} from 'react-router-dom'
 export default function Home() {
   return (
     <div className='home'>
-      <h1>Welcome to <span>Chatscrum</span></h1>
+      <h1>Welcome to Chatscrum</h1>
       <div className="links">
         <h4><Link to='/signin'>Sign in</Link></h4>
         <h4><Link to='/signup'>Sign up</Link></h4>
