5e50a5a3db7d6a91746a3f572a302ba09dbf25c0
James Yaro <onuhjamesyaro@gmail.com>
2022-09-15 22:59:59 +0100

    add routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import SignUp from './components/sign-up/sign-up';
 import SignIn from './components/sign-in/sign-in';
 import Home from './components/home/home';
@@ -7,12 +8,18 @@ import Home from './components/home/home';
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <h1 className='logo'>Chatscrum</h1>
-        <SignUp />
-        <SignIn/>
-        <Home />
-      </div>
+      <BrowserRouter>
+
+        <div className="App">
+          <h1 className='logo'>Chatscrum</h1>
+          <Routes>
+            <Route exact path='/' component={Home} />
+            <Route path='/signin' component={SignIn} />
+            <Route path='/signup' component={SignUp} />
+          </Routes>
+        </div>
+
+      </BrowserRouter>
     )
   }
 }
