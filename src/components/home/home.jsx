43493fb401b60c1acd7cdee73022e37f8c8e500e
James Yaro <onuhjamesyaro@gmail.com>
2022-09-15 22:04:13 +0100

    build home component and style it

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -3,10 +3,12 @@ import './home.css'
 
 export default function Home() {
   return (
-    <div className='links'>
-        <h1>Welcome to Chatscrum</h1>
+    <div className='home'>
+      <h1>Welcome to <span>Chatscrum</span></h1>
+      <div className="links">
         <h4>Sign up</h4>
         <h4>Sign in</h4>
+      </div>
     </div>
   )
 }
