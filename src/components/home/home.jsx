2998590a598621d7347cb914b32a0a0994e7f804
James Yaro <onuhjamesyaro@gmail.com>
2022-09-15 23:27:48 +0100

    add Link to the links

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import './home.css'
+import {Link} from 'react-router-dom'
 
 export default function Home() {
   return (
     <div className='home'>
       <h1>Welcome to <span>Chatscrum</span></h1>
       <div className="links">
-        <h4>Sign up</h4>
-        <h4>Sign in</h4>
+        <h4><Link to='/signin'>Sign in</Link></h4>
+        <h4><Link to='/signup'>Sigup</Link></h4>
       </div>
     </div>
   )
