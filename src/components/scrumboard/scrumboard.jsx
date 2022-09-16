fb3e02e69be591fcc848e44f35bda177a077631c
James Yaro <onuhjamesyaro@gmail.com>
2022-09-16 13:18:09 +0100

    dynamic rendering of scrumboard component

--- src/components/scrumboard/scrumboard.jsx
+++ src/components/scrumboard/scrumboard.jsx
@@ -1,10 +1,25 @@
 import React, { Component } from 'react'
 import './scrumboard.css'
+import data from '../../static/Data'
 
 export class Scrumboard extends Component {
+
+    state = data
+    
   render() {
+    console.log('Logged in as', data.fullname)
     return (
-      <div>Scrumboard</div>
+      <div className='scrumboard'>
+        <nav>
+            <h1>Chatscrum</h1>
+            <div>
+                <p>User type: {data.usertype}</p>
+                <p>Project Name: {data.projectName}</p>
+            </div>
+        </nav>
+
+        <p id="info">Hello {data.fullname}! Welcome to your scrumboard</p>
+      </div>
     )
   }
 }
