c2bf53cea57020fa9490be0709a3969caf340fb8
James Yaro <onuhjamesyaro@gmail.com>
2022-09-20 21:10:48 +0100

    create dummy data for card

---
+++ src/static/tasks.js
@@ -0,0 +1,9 @@
+const taskList = [
+    {id: "one", item: "build chatscrum"},
+    {id: "two", item: "build trello"},
+    {id: "three", item: "go swimming"},
+    {id: "four", item: "play Call of Duty"},
+    {id: "five", item: "sleep all day"}
+]
+
+export default taskList
