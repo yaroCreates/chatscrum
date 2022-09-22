38af3c844180663be8cecc7106c1b1f0b3680ba5
James Yaro <onuhjamesyaro@gmail.com>
2022-09-23 00:11:10 +0100

    rendering user data from api

--- src/components/users/users.jsx
+++ src/components/users/users.jsx
@@ -12,11 +12,27 @@ export class Users extends Component {
         }
     }
 
+    componentDidMount() {
+        axios.get('http://liveapi.chatscrum.com/scrum/api/scrumusers/')
+            .then(res => this.setState({
+                users: res.data.slice(0,8)
+            }))
+            
+    }
+    
 
   render() {
+    console.log(this.state.users)
     return (
-      <div>
-
+      <div className='main'>
+        <h4>Connected Users</h4>
+        <div>
+            {this.state.users.map(({nickname, id}) => (
+                <div className="user" key={id}>
+                    {nickname}
+                </div>
+            ))}
+        </div>
       </div>
     )
   }
