bb82521c06ec2f0002e2c6c371f456d4c43b2afa
James Yaro <onuhjamesyaro@gmail.com>
2022-09-22 18:44:06 +0100

    create user component

--- src/components/users/users.jsx
+++ src/components/users/users.jsx
@@ -2,10 +2,20 @@ import React, { Component } from 'react'
 import './users.css'
 
 export class Users extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            users: [],
+            loading: true
+        }
+    }
+
+
   render() {
     return (
       <div>
-        
+
       </div>
     )
   }
