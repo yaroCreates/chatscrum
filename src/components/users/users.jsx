f138408011878238ea8c376836a6c3e81db42bfc
James Yaro <onuhjamesyaro@gmail.com>
2022-09-22 16:00:30 +0100

    create user component and style

---
+++ src/components/users/users.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './users.css'
+
+export class Users extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Users
